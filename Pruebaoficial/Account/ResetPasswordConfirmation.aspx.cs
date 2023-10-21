ff06e403a5874c4d1ddda7570e91871b78141097
Aprendiz <Aprendiz@BOGDFPCSRFSP157>
2023-10-20 19:56:08 -0500

    Agregar archivos de proyecto.

---
+++ Pruebaoficial/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Pruebaoficial.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Pruebaoficial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pruebaoficial.Global" Language="C#" %>
